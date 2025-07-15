1b16adae342aa7edba173fa524710fa854344e9d
pendoyohbe <pendoyohbe@gmail.com>
2025-07-15 16:20:21 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Viral Sepong';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Sepong Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
-export const url = 'https://viralsepong.pages.dev';
-export const terbit = '2025-01-26T00:01:03Z';
